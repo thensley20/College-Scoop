61c47a9d3ce5c310f944aa7201d42967d814e686
Tyler Hensley <thens417@gmail.com>
2014-04-02 23:57:27 -0400

    Update server.py

--- server.py
+++ server.py
@@ -19,33 +19,24 @@ def page():
 def another_page():
   
     print('anotherpage')
-    scoop = {'postername': MySQLdb.escape_string(request.form['postername']),
-               'activity': MySQLdb.escape_string(request.form['activity']),
+    scoop = {'postername': request.form['postername'],
+               'activity': request.form['activity'],
                'rank': request.form['rank']
-             
                }
     
     if request.method == 'POST':
         db = utils.db_connect()
         cur = db.cursor(cursorclass=MySQLdb.cursors.DictCursor)
-        
-        query = "INSERT INTO club_name (postername) VALUES ('" + MySQLdb.escape_string(request.form['postername']) + "')"
+        query = "INSERT INTO collegescoop_tbl VALUES ('" + request.form['postername'] + "', '" + request.form['activity'] + "', '" + request.form['rank'] + "')"
         # Print query to console (useful for debugging)
         print query
         cur.execute(query)
-        #db.commit()
-        
-        query2 = "INSERT INTO activity (activity, rank) VALUES ('" + MySQLdb.escape_string(request.form['activity']) + "', '" + request.form['rank'] + "')"
-        # Print query to console (useful for debugging)
-        print query2
-        cur.execute(query2)
         db.commit()
         
-        
-    cur.execute('SELECT DISTINCT cn.postername, a.activity, a.rank FROM club_name cn JOIN activity a')
+    cur.execute('select * from collegescoop_tbl')
     rows = cur.fetchall()
 
-    return render_template('another_page.html', club_name=rows, activity = rows, scoop = scoop)
+    return render_template('another_page.html', collegescoop_tbl = rows, scoop = scoop)
 
   
 @app.route('/another_page2', methods=['GET', 'POST'])
@@ -54,18 +45,18 @@ def another_page2():
     db = utils.db_connect()
     cur = db.cursor(cursorclass=MySQLdb.cursors.DictCursor)
       
-    query = 'SELECT DISTINCT cn.postername, a.activity, a.rank FROM club_name cn JOIN activity a'
+    query = 'SELECT * from collegescoop_tbl'
     
     cur.execute(query)
     rows = cur.fetchall()
     
 
-    #cur.execute('SELECT cn.postername, a.activity, a.rank FROM club_name cn JOIN activity a')
-    #rows = cur.fetchall()
+    cur.execute('select * from collegescoop_tbl')
+    rows = cur.fetchall()
   
     
     
-    return render_template('another_page2.html', club_name=rows, activity=rows, selectedMenu='List')
+    return render_template('another_page2.html', collegescoop_tbl=rows, selectedMenu='List')
 
 @app.route('/login', methods = ['GET','POST'])
 def login():
@@ -85,7 +76,7 @@ def register():
     if request.method == 'POST':
         db = utils.db_connect()
         cur = db.cursor(cursorclass=MySQLdb.cursors.DictCursor)
-        query = "INSERT INTO users (username, password) VALUES ('" + MySQLdb.escape_string(request.form['ruser']) + "', '" + MySQLdb.escape_string(request.form['rpass']) + "')"
+        query = "INSERT INTO userscoop_tbl VALUES ('" + request.form['ruser'] + "', '" + request.form['rpass'] + "')"
         # Print query to console (useful for debugging)
         print query
         cur.execute(query)
